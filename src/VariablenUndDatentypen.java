fe3f158c606ea4cfd167816f67b522c9e32ec71b
Raphaela Dohrmann <raphaela.dohrmann@gfn.de>
2025-11-24 12:48:49 +0100

    Test

---
+++ src/VariablenUndDatentypen.java
@@ -0,0 +1,20 @@
+public class VariablenUndDatentypen {
+    public static void main(String[] args) {
+        // Primitiven Datentypen
+        // Ganzzahlen
+        byte anzahlProdukte = 5;
+        short maximalerBestellwert = 1000;
+        int kundenAlter = 33;
+        long kundenID = 1567238775L;
+
+        // Gleitkommazahlen
+        float produktBewertung = 4.5f;
+        double warenKorbWert = 59.99;
+
+        boolean istVersandkostenFrei = true;
+        char kundenGeschlecht = 'M'; // Kunde ist ein Mann -> "M"
+
+        String name = "Raphaela";
+        System.out.println(kundenAlter);
+    }
+}
