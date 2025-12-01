d7372b927d4eb28164058667bbad549cd9aafe3d
Raphaela Dohrmann <raphaela.dohrmann@gfn.de>
2025-12-01 15:21:36 +0100

    OOP Einstieg

--- src/tag6_mehrdimensionale_arrays_arrayList/oop_Einstieg/Hauptprogramm.java
+++ src/tag6_mehrdimensionale_arrays_arrayList/oop_Einstieg/Hauptprogramm.java
@@ -4,9 +4,6 @@ public class Hauptprogramm {
     public static void main(String[] args) {
 
         // 1. Erstellen von Objekten (Instanzen)
-        // Instanzattribute = Variablen, die zu jedem Objekt gehören
-        // Referenzvariable = Variable, die auf ein Objekt zeigt
-        // Referenztyp = Typ der Referenzvariable (z.B. Artikel)
 
         // 2. Zugriff auf Attribute und Methoden
         Artikel artikel = new Artikel();
