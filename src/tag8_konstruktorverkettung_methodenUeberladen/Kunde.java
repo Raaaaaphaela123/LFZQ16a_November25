85adba9078d3bd3564e8a1df7009578324d54993
Raphaela Dohrmann <raphaela.dohrmann@gfn.de>
2025-12-03 10:25:18 +0100

    Tag 8 Konstruktorverkettung, MEthoden überladen

---
+++ src/tag8_konstruktorverkettung_methodenUeberladen/Kunde.java
@@ -0,0 +1,61 @@
+package tag8_konstruktorverkettung_methodenUeberladen;
+
+public class Kunde {
+    // Instanzattribute
+    private String vorname = "Unbekannt";
+    public String nachname = "Unbekannt";
+    private double guthaben = 0;
+    private int alter;
+
+    /* Konstruktorverkettung */
+    // Standardkonstruktor (ohne Parameter)
+    public Kunde() {
+        this("Unbekannter Vorname", "Unbekannter Nachname", 0);
+    }
+
+    // Konstruktor mit 3 Parametern (Endpunkt der Verkettung)
+    public Kunde(String vorname, String nachname, double guthaben) {
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.guthaben = guthaben;
+        System.out.println("Konstruktor mit 3 Parametern ausgeführt");
+    }
+
+
+    // Methode ohne Parameter / ohne Rückgabe
+    public void anzeigen() {
+        System.out.println("Vorname: " + vorname + ", Nachname: " + nachname + ", Guthaben: " + guthaben);
+    }
+
+    // Methode mit Parameter / mit Rückgabe
+    public void einkaufen(double betrag) {
+        if (betrag <= guthaben) {
+            guthaben -= betrag;
+            System.out.println(betrag + " € wurden abgezogen. Restbetrag Guthaben: " + guthaben);
+        } else {
+            System.out.println("Nicht genug Guthaben für den Betrag von " + betrag + " €.");
+        }
+    }
+
+    // Getter und Setter
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public double getGuthaben() {
+        return guthaben;
+    }
+
+    public void setGuthaben(double guthaben) {
+        this.guthaben = guthaben;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+}
