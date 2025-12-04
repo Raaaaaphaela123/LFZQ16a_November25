aff2da1ae53dc70643b485822faa8100abd8bb87
Raphaela Dohrmann <raphaela.dohrmann@gfn.de>
2025-12-04 10:42:28 +0100

    Tag 9 - Vererbung, static, Überschreiben, toString(), super()

---
+++ src/tag10_vererbung_mit_extends/Premiumkunde.java
@@ -0,0 +1,20 @@
+package tag10_vererbung_mit_extends;
+
+public class Premiumkunde extends Kunde {
+    // Instanzavriablen
+    private double rabatt; // zusätzlich Attribut zur Oberklasse
+
+    // Konstruktor der Unterklasse
+    public Premiumkunde(String vorname, String nachname, double guthaben, double rabatt) {
+        // ruft den Konstruktor der Basisklasse auf, auf den die mitgegebenen Parameter passen
+        // es muss ein passender Konstruktur in der Elternklasse definiert sein
+        super(vorname, nachname, guthaben);
+        this.rabatt = rabatt;
+    }
+
+    // Methoden der Elternklasse wird überschrieben und mit super wird die Implementierung der Elternklasse genutzt und dann hier ergänzt
+    @Override
+    public String toString() {
+        return super.toString() + ", Rabatt: " + rabatt;
+    }
+}
